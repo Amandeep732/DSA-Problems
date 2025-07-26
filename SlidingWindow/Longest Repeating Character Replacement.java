623220658aa213836d718fd125b6909354299964
Amandeep Singh <achalgaming9722gmail.com>
2025-07-26 18:37:10 +0530

    added Sliding window

---
+++ SlidingWindow/Longest Repeating Character Replacement.java
@@ -0,0 +1,26 @@
+package SlidingWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+public class LongestRepeatingCharacterReplacement {
+      public int characterReplacement(String s, int k) {
+      Map<Character, Integer> map = new HashMap<>();
+      int ans =0;
+      int maxCount = 0;
+      int left = 0;
+
+      for(int r = 0; r < s.length(); r++){
+        char ch = s.charAt(r);
+        map.put(ch, map.getOrDefault(ch, 0)+1);
+        maxCount = Math.max(maxCount, map.get(ch));
+
+        if(r - left + 1 - maxCount > k){
+          char leftchar = s.charAt(left);
+          map.put(leftchar, map.get(leftchar)-1);
+          left++;
+        }
+        ans = Math.max(ans, r - left + 1);
+      } 
+      return ans;
+    }
+}
